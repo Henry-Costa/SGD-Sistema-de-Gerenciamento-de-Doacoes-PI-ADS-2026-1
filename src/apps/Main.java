a7c8688b82f837494c89c662a003da104cd53867
Henry Costa <henryv.costa38@gmail.com>
2026-05-27 23:37:39 -0300

    Tela de login e cadastrar usuário funcional localmente (Ainda sem conexão ao banco de dados), tela de menu inicial para testes

---
+++ src/apps/Main.java
@@ -0,0 +1,12 @@
+package apps;
+
+import telas.TelaLogin;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new TelaLogin().setVisible(true);
+
+    }
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,7 @@
  * 
  */
 module SGDSistemadeGerenciamentodeDoacoes {
+
+    requires java.desktop;
+
 }
